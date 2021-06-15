66cade7a0787d078a63597eafe5f24806a0256dd
Satenik <karapetyanashkhen@gmail.com>
2021-06-15 16:45:05 +0200

    Add project files.

---
+++ EmployeeRegister/EmployeeList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeRegister
+{
+    class EmployeeList
+    {
+        private readonly List<Employee> employees;
+
+        public EmployeeList()
+        {
+            employees = new List<Employee> ();
+        }
+
+        public void AddEmployee(Employee employee)
+        {
+            employees.Add(employee);
+        }
+
+        public List<Employee> DisplayEmployeeList()
+        {
+            return employees;
+        }
+
+        
+
+        
+    }
+}
---
+++ EmployeeRegister/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace EmployeeRegister
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            EmployeeList list = new EmployeeList();
+
+            Console.WriteLine("Please type Add if you want to add an employee or Display if you want to see the employee list or Exit to exit.");
+            string input ;
+
+            do
+            {
+                input = Console.ReadLine();
+                if (input.Equals("Add"))
+                {
+                    Employee employee = new Employee();
+                    Console.WriteLine("Enter the name of employee.");
+                    employee.Name = Console.ReadLine();
+                    Console.WriteLine("Enter the salary of employee.");
+                    employee.Salary = int.Parse(Console.ReadLine());
+                    list.AddEmployee(employee);
+                }
+                else
+                {
+                    if (input.Equals("Display"))
+                    {
+                        if (list.Equals(null))
+                            Console.WriteLine("List is empty.");
+                        else
+                        {
+                            list.DisplayEmployeeList();
+                            foreach (Employee item in list.DisplayEmployeeList().ToArray())
+                            {
+                                Console.WriteLine(item.Name + " " + item.Salary);
+                            }
+                        }
+                    }
+                    
+                }
+            } while (!input.Equals("Exit"));
+        }
+    }
+}
